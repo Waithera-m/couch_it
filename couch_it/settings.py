e295df524356ae845c0f4813d7f0659e9bde0f31
Mary Waithera <njihiamary11@gmail.com>
2020-05-27 16:25:45 +0300

    create view function to get popular and upcoming movies and create path to template that displays these categories

--- couch_it/settings.py
+++ couch_it/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
+    'tmdbsimple',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import tmdbsimple as tmdb
+import os
+from decouple import config
 
 # Create your views here.
+tmdb.API_KEY=config('API_KEY')
+print(tmdb.API_KEY)
+
+def index(request):
+    """
+    view function returns template that displays popular and upcoming movies
+    """
+    popular_movies_tmdb = tmdb.Movies('popular')
+    popular_movies = popular_movies_tmdb.info()['results']
+
+    upcoming_movies_tmdb = tmdb.Movies('upcoming')
+    upcoming_movies = upcoming_movies_tmdb.info()['results']
+
+    return render(request, 'movies.html', {"popular":popular_movies, 'upcoming':upcoming_movies})
