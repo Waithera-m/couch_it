eef075c76f89d92feaabefd94f256cab2aa66621
Mary Waithera <njihiamary11@gmail.com>
2020-05-29 14:05:24 +0300

    create admin page and profile model to facilitate the creation of profile objects

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    """
+    class facilitates the creation of profile objects
+    """
+    username = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to="images", null=True)
