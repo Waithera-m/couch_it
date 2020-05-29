7c125c581731af8febd433ebc7bc1e7e9bd30eb5
Mary Waithera <njihiamary11@gmail.com>
2020-05-29 14:14:57 +0300

    Add configurations to facilitate image uploading and the storage of upload profile images

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'movies'
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('movie/<int:movie_id>', views.single_movie, name="single_movie"),
     path('show/<int:tv_id>', views.single_show, name="single_show"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
